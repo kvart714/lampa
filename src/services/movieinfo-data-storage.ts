e5175c6f8578e92811173b9e09ae60eb53271641
lex <lexandros71@gmail.com>
2026-01-02 21:31:04 +0400

    FIX. Incorrect information in the movie card, and the TMDB API reject with real email in request.

--- src/services/movieinfo-data-storage.ts
+++ src/services/movieinfo-data-storage.ts
@@ -1,5 +1,6 @@
 import { warn } from '../log'
 import manifest from '../manifest.json'
+import { log } from '../log'
 
 export const MOVIE_INFO_STORAGE_KEY = manifest.component + '.movieinfo.data.v3'
 
@@ -28,9 +29,12 @@ export class MovieInfoDataStorage {
     }
 
     private static async loadByTypeWithFallback(id: number, type?: MovieType): Promise<void> {
+/*fix by lexandr0s.* Searching by array returns incorrect results if the database contains both TV and Movie with the same ID.
         let typesToTry: MovieType[] = ['movie', 'tv']
 
         for (const contentType of typesToTry.sort((a) => a === type ? -1 : 0)) {
+*/
+            let contentType = type //*fix by lexandr0s.*
             const info = await this.loadContentInfo(id, contentType)
             if (info) {
                 this.memoryCache[id] = info
@@ -38,14 +42,13 @@ export class MovieInfoDataStorage {
                 Lampa.Storage.set(MOVIE_INFO_STORAGE_KEY, this.diskCache)
                 return
             }
-        }
+      //} //*fix by lexandr0s.*
     }
 
     private static async loadContentInfo(id: number, contentType: MovieType): Promise<MovieInfo | null> {
         const lang = Lampa.Storage.field('tmdb_lang') || Lampa.Storage.field('language') || 'ru'
-
         const url = Lampa.Utils.addUrlComponent(
-            Lampa.TMDB.api(`${contentType}/${id}`),
+            Lampa.TMDB.api(`${contentType}/${id}?email=`), //Fix by lexandr0s. Add an empty email to the URL to prevent TMDB API rejection.
             `api_key=${Lampa.TMDB.key()}&language=${lang}&certification_country=ru&certification.lte=18`
         )
 
